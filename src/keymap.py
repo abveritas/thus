d1e1276cf82c14fd6f39fdfb7dc6d3809ef791ec
Philip <philm@manjaro.org>
2014-01-09 17:55:51 +0100

    Bugfix - fix typo

--- src/keymap.py
+++ src/keymap.py
@@ -56,7 +56,7 @@ class Keymap(Gtk.Box):
         self.keyboard_test_entry = self.ui.get_object("keyboard_test_entry")
 
         self.keyboard_box = self.ui.get_object("keyboard_box")
-        self.keyboard_image = generate_keyboard_layout.Keyboard()
+        self.keyboard_image = keyboard_widget.Keyboard()
         self.keyboard_box.add(self.keyboard_image)
 
         self.create_toolviews()
